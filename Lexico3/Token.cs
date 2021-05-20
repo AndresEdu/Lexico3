0eb095450bf48d797c3596d4c1f7aa8177e9a8d9
Andrés Arciniega <78604874+AndresEdu@users.noreply.github.com>
2021-05-19 19:25:55 -0500

    Add files via upload

---
+++ Lexico3/Token.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexico3
+{
+    class Token
+    {
+        public enum Clasificaciones
+        {
+            identificador, numero, asignacion, inicializacion, caracter,
+            FinSentencia, OperadorLogico, OperadorRelacional, OperadorTermino,
+            OperadorFactor, IncrementoTermino, IncrementoFactor, Cadena, OperadorTernario,
+            TipoDato, Zona, Condicion, ciclo, IniciodeBloque, FindeBloque,
+        }
+        private string contenido;
+        private Clasificaciones clasificacion;
+
+        public void SetContenido(string contenido)
+        {
+            this.contenido = contenido;
+        }
+
+        public void SetClasificacion(Clasificaciones clasificacion)
+        {
+            this.clasificacion = clasificacion;
+        }
+
+        public string GetContenido()
+        {
+            return contenido;
+        }
+        public Clasificaciones GetClasificacion()
+        {
+            return clasificacion;
+        }
+    }
+}
